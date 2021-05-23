507a3174a3664d70c55f25f1c3866ae7c052cdf9
Ivan <ggwpdiaf@gmail.com>
2021-05-23 20:35:40 -0300

    added some tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,15 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+
+import { server } from "./mocks/server";
+
+// Establish API mocking before all tests.
+beforeAll(() => server.listen());
+
+// Reset any request handler that we may add during test, so they don't affect others
+afterEach(() => server.resetHandlers());
+
+// Clean up after the tests are finished
+afterAll(() => server.close());
